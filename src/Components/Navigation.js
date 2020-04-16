42848c08d4eeebbcdd78e24042299e9fe7dae00a
Deep kakadia <kakadiadeepus@gmail.com>
2020-04-15 23:52:51 -0400

    "Running Code"

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,35 +1,34 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-//import SignOutButton from "./SignOut";
-import firebase from 'firebase'
-const Navigation = () => (
-    <div>
-        {firebase.isAuthenticated() ? <NavigationAuth /> : <NavigationNonAuth />}
-    </div>
-);
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../firebase/Auth';
+import SignOutButton from './SignOut';
+import '../App.css';
+
+const Navigation = () => {
+    const { currentUser } = useContext(AuthContext);
+    return <div>{currentUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
+};
 
 const NavigationAuth = () => (
     <nav className="navigation">
         <ul>
             <li>
-                <NavLink exact to="/" activeClassName="active">
+                <NavLink exact to="/water" activeClassName="active">
                     Water
-        </NavLink>
+                </NavLink>
             </li>
             <li>
-                <NavLink to="/home" activeClassName="active">
-                    Home
-        </NavLink>
+                <NavLink to="/foodMain" activeClassName="active">
+                    Foodpage
+                 </NavLink>
             </li>
             <li>
                 <NavLink exact to="/account" activeClassName="active">
                     Account
-        </NavLink>
+                </NavLink>
             </li>
             <li>
-                <NavLink exact to="/account" activeClassName="active">
-                    Foodpage
-        </NavLink>
+                <SignOutButton />
             </li>
 
         </ul>
@@ -40,14 +39,19 @@ const NavigationNonAuth = () => (
     <nav className="navigation">
         <ul>
             <li>
-                <NavLink exact to="/water" activeClassName="active">
-                    Water
-        </NavLink>
+                <NavLink exact to='/' activeClassName='active'>
+                    Landing
+					</NavLink>
             </li>
             <li>
-                <NavLink to="/foodMain" activeClassName="active">
-                    Foodpage
-        </NavLink>
+                <NavLink exact to='/signin' activeClassName='active'>
+                    Sign-In
+					</NavLink>
+            </li>
+            <li>
+                <NavLink exact to='/signup' activeClassName='active'>
+                    Sign-up
+					</NavLink>
             </li>
         </ul>
     </nav>
--- src/firebase/FirebaseFunctions.js
+++ src/firebase/FirebaseFunctions.js
@@ -1,10 +1,15 @@
 import firebase from 'firebase/app';
 
-
 async function doCreateUserWithEmailAndPassword(email, password, displayName) {
     await firebase.auth().createUserWithEmailAndPassword(email, password);
     firebase.auth().currentUser.updateProfile({ displayName: displayName });
-    console.log(`DISPLAY NAME ${displayName}`);
+}
+
+async function doChangePassword(email, oldPassword, newPassword) {
+    let credential = firebase.auth.EmailAuthProvider.credential(email, oldPassword);
+    await firebase.auth().currentUser.reauthenticateWithCredential(credential);
+    await firebase.auth().currentUser.updatePassword(newPassword);
+    await doSignOut();
 }
 
 async function doSignInWithEmailAndPassword(email, password) {
@@ -13,9 +18,9 @@ async function doSignInWithEmailAndPassword(email, password) {
 
 async function doSocialSignIn(provider) {
     let socialProvider = null;
-    if (provider === "google") {
+    if (provider === 'google') {
         socialProvider = new firebase.auth.GoogleAuthProvider();
-    } else if (provider === "facebook") {
+    } else if (provider === 'facebook') {
         socialProvider = new firebase.auth.FacebookAuthProvider();
     }
     await firebase.auth().signInWithPopup(socialProvider);
@@ -39,5 +44,6 @@ export {
     doSignInWithEmailAndPassword,
     doPasswordReset,
     doPasswordUpdate,
-    doSignOut
+    doSignOut,
+    doChangePassword
 };
