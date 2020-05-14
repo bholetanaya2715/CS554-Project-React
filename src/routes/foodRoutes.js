c0f77a6531654b3e8a8d980c66a3d989b8d687d4
Sejal Vyas <sejalvyas13@gmail.com>
2020-05-14 19:12:08 -0400

    Changes for target

--- src/routes/foodRoutes.js
+++ src/routes/foodRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const data = require("../data");
-const food = data.food;
+const food = require("../data/food");
 
 router.post("/", async (req, res) => {
     let foodInfo = req.body;
@@ -10,12 +10,12 @@ router.post("/", async (req, res) => {
 
     if(typeof foodInfo != 'object' ||  foodInfo == null) throw 'Invalid input exception'
 
-
     try {
-        const updatedObj = await food.postFoodById(foodInfo.id,foodInfo.foodArr)
+        const updatedObj = await food.postFoodById(foodInfo.id,foodInfo.foodArr, foodInfo.target)
         console.log(updatedObj)
         res.json(updatedObj);
     } catch (e) {
+        console.log(e)
         res.sendStatus(500);
     }
 });
@@ -30,6 +30,40 @@ router.get("/:id", async (req, res) => {
     catch (e) {
         console.log("Server Error is " + e);
         res.status(500).json(e)
+    }
+})
+
+router.post("/updateTarget", async (req, res) => {
+    let foodInfo = req.body;
+    console.log("Post in food route is hit");
+    console.log(foodInfo);
+
+    if(typeof foodInfo != 'object' ||  foodInfo == null) throw 'Invalid input exception'
+
+    try {
+        const updatedObj = await food.updateTarget(foodInfo.id, foodInfo.target)
+        console.log(updatedObj)
+        res.json(updatedObj);
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500);
+    }
+})
+
+router.post("/updateCurrent", async (req, res) => {
+    let foodInfo = req.body;
+    console.log("Post in food route is hit");
+    console.log(foodInfo);
+
+    if(typeof foodInfo != 'object' ||  foodInfo == null) throw 'Invalid input exception'
+
+    try {
+        const updatedObj = await food.updateCurrent(foodInfo.id, foodInfo.current)
+        console.log(updatedObj)
+        res.json(updatedObj);
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500);
     }
 })
 
