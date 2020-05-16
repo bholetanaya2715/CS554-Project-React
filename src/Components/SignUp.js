b90338f38b2a23bb7f55788c371ccca808ed50cb
Deep kakadia <kakadiadeepus@gmail.com>
2020-05-16 13:29:05 -0400

    Token for backend

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -51,8 +51,18 @@ function SignUp() {
 
     try {
       await doCreateUserWithEmailAndPassword(email.value, passwordOne.value, displayName.value)
+      let token = await currentUser.getIdToken()
+      const { temp } = await Axios.post("http://localhost:8000/api/adduser", {
+        email: email.value, userName: displayName.value,
+        headers: {
+          'accept': 'application/json',
+          'Accept-Language': 'en-US,en;q=0.8',
+          'Content-Type': 'multipart/form-data',
+          'authtoken': token
+        }
+      });
 
-      let temp = await Axios.post("http://localhost:8000/api/adduser", { email: email.value, userName: displayName.value })
+      // let temp = await Axios.post("http://localhost:8000/api/adduser", { email: email.value, userName: displayName.value })
       console.log(temp)
 
     } catch (error) {
