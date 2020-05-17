fe365f534d1040c317a3329898bf932d476cac3d
Deep kakadia <kakadiadeepus@gmail.com>
2020-05-17 12:51:21 -0400

    button update

--- src/Components/SignOut.js
+++ src/Components/SignOut.js
@@ -5,12 +5,12 @@ import Button from "react-bootstrap/Button";
 import { Redirect } from "react-router-dom";
 
 const SignOutButton = () => {
-  return (
-    <a href="/signin">
-      <button type="button" onClick={doSignOut} className="showlink">
-        Sign Out
+    return (
+        <a href="/signin">
+            <button type="button" onClick={doSignOut} className="showlink">
+                Sign Out
       </button>
-    </a>
-  );
-
+        </a>
+    );
+}
 export default SignOutButton;
