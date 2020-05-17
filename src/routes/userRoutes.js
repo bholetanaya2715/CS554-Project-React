76ba446334712d63835cd35c46cdb08748726179
malav <malavshah2018gmail.com>
2020-05-17 17:21:00 -0400

    Created pdf api and component

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,11 @@ const userMethods = data.users;
 const accountMethods = data.account;
 const waterMethods = data.water;
 const foodHistory = data.history;
-const checkAuth = require("./checkAuth");
+const pdf = data.pdf;
+var path = require('path');
+const checkAuth = require("./checkAuth")
+const fs = require("fs")
+const del = require('del');
 
 router.post("/adduser", async (req, res) => {
   console.log(req.body.email);
@@ -101,5 +105,39 @@ router.get("/foodHistory/:id", async (req, res) => {
     res.status(500).json(e);
   }
 });
+
+/**
+ * Get user's diet history
+ */
+router.post('/pdf/:id', async (req, res) => {
+  const id = req.params.id;
+  let resPath = path.join(__dirname, "../GeneratedPDF/" + id + '_history.pdf')
+
+  try {
+    const deletedPaths = await del([resPath]);
+
+    // console.log('Deleted files and directories:\n', deletedPaths.join('\n'));
+
+    await pdf.pdfDoc(id);
+    res.json('Success');
+  } catch (e) {
+    console.log('e', e)
+  }
+
+})
+
+router.get('/pdf/:id', async (req, res) => {
+  var id = req.params.id;
+  let resPath = path.join(__dirname, "../GeneratedPDF/" + id + '_history.pdf')
+  try {
+    if (fs.existsSync(resPath)) {
+      res.sendFile(resPath)
+    } else {
+      throw "File does not exist."
+    }
+  } catch (e) {
+    console.error(e)
+  }
+})
 
 module.exports = router;
